b6b18a3f0097bd70ec8542e863ebdae282c07aa9
mohammedrakib944 <mohammedrakib944.com>
2025-09-10 23:07:01 +0600

    GSAP animation

--- GSAP-website/script.js
+++ GSAP-website/script.js
@@ -1,7 +1,7 @@
+// LOGO SECTION
 gsap.from("#logo", {
   y: -160,
   scale: 8,
-  color: "white",
   scrollTrigger: {
     trigger: ".navbar",
     start: "top bottom",
@@ -10,16 +10,62 @@ gsap.from("#logo", {
   },
 });
 
-gsap.to(".content h1", {
-  y: 200,
-  scale: 0.5,
-  opacity: 0,
-  stagger: true,
+// CONTENT SECTION
+gsap.from(".content h1", {
   scrollTrigger: {
     trigger: ".content",
-    start: "50% 50%",
-    end: "50% 50%",
-    scrub: 2,
-    markers: true,
+    start: "top 50%",
+    toggleActions: "play reverse play reverse",
   },
+  y: 80,
+  opacity: 0,
+  stagger: 0.4,
+  ease: "power3.out",
+});
+
+// Horizontal scroll animation
+let photos = document.querySelector(".photos");
+let photosWidth = photos.scrollWidth;
+let viewportWidth = window.innerWidth;
+let scrollDistance = photosWidth - viewportWidth + 300;
+
+gsap.to(photos, {
+  x: -scrollDistance,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".photos-wrapper",
+    start: "40% 40%",
+    end: () => `+=${scrollDistance}`,
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
+  },
+});
+
+// TAGS SECTION
+gsap.from(".tags li", {
+  scrollTrigger: {
+    trigger: ".tags",
+    start: "top 60%",
+    toggleActions: "play reverse play reverse",
+  },
+  y: 40,
+  rotate: 10,
+  opacity: 0,
+  stagger: 0.2,
+  ease: "power2.out",
+});
+
+// Footer SECTION
+gsap.from(".footer", {
+  scrollTrigger: {
+    trigger: ".footer",
+    start: "top 90%",
+    end: "bottom 60%",
+    toggleActions: "play reverse play reverse",
+  },
+  y: 150,
+  duration: 5,
+  opacity: 0,
+  ease: "elastic.out(1,0.3)",
 });
